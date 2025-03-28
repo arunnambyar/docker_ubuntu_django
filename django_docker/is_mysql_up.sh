9c902baf44e7330cfee0d6b17cddcb4139848927
Arun Mangatt <arunnambyar@gmail.com>
2025-03-28 16:15:10 +0530

    Django was starting before mysql is fully up. docker-compose depends_on don't have any usage here. so creating a bash script

---
+++ django_docker/is_mysql_up.sh
@@ -0,0 +1,15 @@
+DB_HEALTH=$(python manage.py check --database default)
+COUNTER=0
+
+while [[ ${COUNTER} -lt 10 && "$DB_HEALTH" != "System check identified no issues (0 silenced)." ]]
+do
+  sleep 2
+  echo "REPEAT - $DB_HEALTH, $COUNTER"
+  let "COUNTER++"
+  DB_HEALTH=$(python manage.py check --database default)
+done
+
+echo "MySQL Connection established successfully"
+
+echo "Starting django DEV server..."
+python manage.py runserver 0.0.0.0:9000
--- django_docker/manage.py
+++ django_docker/manage.py
@@ -15,8 +15,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    import time
-    time.sleep(10)
+    # import time
+    # time.sleep(10)
     execute_from_command_line(sys.argv)
 
 
